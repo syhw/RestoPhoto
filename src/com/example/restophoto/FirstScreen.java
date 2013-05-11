d0bffc91419a46171f7284b4401d10601622310a
syhw <gabriel.synnaeve@gmail.com>
2013-05-11 21:18:26 +0200

    working on photos/takepicture callback

--- src/com/example/restophoto/FirstScreen.java
+++ src/com/example/restophoto/FirstScreen.java
@@ -2,27 +2,100 @@ package com.example.restophoto;
 
 import java.io.File;
 import java.io.FileOutputStream;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import android.app.Activity;
+import android.content.Intent;
 import android.graphics.ImageFormat;
 import android.hardware.Camera;
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
+import android.view.View;
 import android.view.Window;
 import android.widget.Toast;
 
 public class FirstScreen extends Activity {
 	public static String DEBUG_TAG;
+	
+	/*
+	private static final int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 100;
+	private Uri fileUri;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_take_pic);
+
+		// create Intent to take a picture and return control to the calling application
+		Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+		fileUri = getOutputPhotoFileUri(); // create a file to save the image
+		intent.putExtra(MediaStore.EXTRA_OUTPUT, fileUri); // set the image file name
+
+		// start the image capture Intent
+		startActivityForResult(intent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
+	}
+
+	private static Uri getOutputPhotoFileUri(){
+		return Uri.fromFile(getOutputPhotoFile());
+	}
+
+	private static File getOutputPhotoFile(){
+		// To be safe, you should check that the SDCard is mounted
+		// using Environment.getExternalStorageState() before doing this.
+
+		File mediaStorageDir = new File(Environment.getExternalStoragePublicDirectory(
+				Environment.DIRECTORY_PICTURES), "MyCameraApp");
+		// This location works best if you want the created images to be shared
+		// between applications and persist after your app has been uninstalled.
+
+		// Create the storage directory if it does not exist
+		if (! mediaStorageDir.exists()){
+			if (! mediaStorageDir.mkdirs()){
+				Log.d("MyCameraApp", "failed to create directory");
+				return null;
+			}
+		}
+
+		// Create a media file name
+		String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+		File mediaFile;
+		mediaFile = new File(mediaStorageDir.getPath() + File.separator + "IMG_"+ timeStamp + ".jpg");
+
+		return mediaFile;
+	}
+	
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+	    if (requestCode == CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE) {
+	        if (resultCode == RESULT_OK) {
+	            // Image captured and saved to fileUri specified in the Intent
+	            Toast.makeText(this, "Image saved to:\n" +
+	                     data.getData(), Toast.LENGTH_LONG).show();
+	        } else if (resultCode == RESULT_CANCELED) {
+	            // User cancelled the image capture
+	        } else {
+	            // Image capture failed, advise user
+	        }
+	    }
+	}
+	*/
+
+	
 	private SurfaceView preview=null;
 	private SurfaceHolder previewHolder=null;
 	private Camera camera=null;
 	private boolean inPreview=false;
-	private boolean cameraConfigured=false;
-
+	private boolean cameraConfigured=false; 
+	 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		requestWindowFeature(Window.FEATURE_NO_TITLE);       
@@ -40,15 +113,15 @@ public class FirstScreen extends Activity {
 		super.onResume();
 
 		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.GINGERBREAD) {
-			/*Camera.CameraInfo info=new Camera.CameraInfo();
-
-      for (int i=0; i < Camera.getNumberOfCameras(); i++) {
-        Camera.getCameraInfo(i, info);
-
-        if (info.facing == Camera.CameraInfo.CAMERA_FACING_FRONT) {
-          camera=Camera.open(i);
-        }
-      }*/
+//			Camera.CameraInfo info=new Camera.CameraInfo();
+//
+//      for (int i=0; i < Camera.getNumberOfCameras(); i++) {
+//        Camera.getCameraInfo(i, info);
+//
+//        if (info.facing == Camera.CameraInfo.CAMERA_FACING_FRONT) {
+//          camera=Camera.open(i);
+//        }
+//      }
 			camera=Camera.open();
 		}
 
@@ -72,24 +145,23 @@ public class FirstScreen extends Activity {
 		super.onPause();
 	}
 
-	/*@Override
-  public boolean onCreateOptionsMenu(Menu menu) {
-    new MenuInflater(this).inflate(R.menu.options, menu);
+	//@Override
+	//public boolean onCreateOptionsMenu(Menu menu) {
+	//  new MenuInflater(this).inflate(R.menu.options, menu);
 
-    return(super.onCreateOptionsMenu(menu));
-  }*/
+	//   return(super.onCreateOptionsMenu(menu));
+	//}
 
-	/*@Override
-  public boolean onOptionsItemSelected(MenuItem item) {
-    if (item.getItemId() == R.id.camera) {
-      if (inPreview) {
-        camera.takePicture(null, null, photoCallback);
-        inPreview=false;
-      }
-    }
-
-    return(super.onOptionsItemSelected(item));
-  }*/
+	//@Override
+	//public boolean onOptionsItemSelected(MenuItem item) {
+	//	if (item.getItemId() == R.id.camera) {
+	//		if (inPreview) {
+	//			camera.takePicture(null, null, photoCallback);
+	//			inPreview=false;
+	//		}
+	//	}
+	//	return(super.onOptionsItemSelected(item));
+	//}
 
 	private Camera.Size getBestPreviewSize(int width, int height,
 			Camera.Parameters parameters) {
@@ -198,8 +270,10 @@ public class FirstScreen extends Activity {
 		@Override
 		protected String doInBackground(byte[]... jpeg) {
 			File photo=
-					new File(Environment.getExternalStorageDirectory(),
-							"photo.jpg");
+					new File(Environment.getExternalStoragePublicDirectory(
+							Environment.DIRECTORY_PICTURES),
+							//Environment.getExternalStorageDirectory(),
+							"photoresto.jpg");
 
 			if (photo.exists()) {
 				photo.delete();
@@ -218,99 +292,14 @@ public class FirstScreen extends Activity {
 			return(null);
 		}
 	}
-}
-
-
-/*
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.List;
-
-import android.os.Bundle;
-import android.provider.MediaStore;
-import android.app.Activity;
-import android.content.Context;
-import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.content.pm.ResolveInfo;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.hardware.Camera;
-import android.util.Log;
-import android.view.Menu;
-import android.view.View;
-import android.widget.FrameLayout;
-import android.widget.ImageView;
-
-public class FirstScreen extends Activity {
-    private static final int REQUEST_CODE = 1;
-	public static String DEBUG_TAG = "Debug tag";
-    private Bitmap bitmap;
-    private ImageView imageView;
-	private Camera camera;
-	private Preview preview;
-
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-      super.onCreate(savedInstanceState);
-      setContentView(R.layout.activity_take_pic);
-      boolean qOpened = false;
-		try {
-			preview = new Preview(this, camera);
-			FrameLayout fl_preview = (FrameLayout) findViewById(R.id.cameraPreview);
-	        fl_preview.addView(preview);
-			releaseCameraAndPreview();
-			camera = Camera.open(); // open without args open the rear facing cam
-			qOpened = (camera != null);
-		} catch (Exception e) {
-			Log.e(getString(R.string.app_name), "failed to open Camera");
-			e.printStackTrace();
-		}
-	}
-
-    private boolean safeCameraOpen() {
-        boolean qOpened = false;
-
-        try {
-            releaseCameraAndPreview();
-            camera = Camera.open();
-            qOpened = (camera != null);
-        } catch (Exception e) {
-            Log.e(getString(R.string.app_name), "failed to open Camera");
-            e.printStackTrace();
-        }
-
-        return qOpened;    
-    }
 
 	public void onClick(View view) {
-		camera.takePicture(null, null,
-				new PhotoHandler(getApplicationContext()));
-	}
-
-	private void releaseCameraAndPreview() {
-		preview.setCamera(null);
-		if (camera != null) {
-			camera.stopPreview();
-			camera.release();
-			camera = null;
+		if (inPreview) {
+			camera.takePicture(null, null, photoCallback);
+				//new PhotoHandler(getApplicationContext()));
 		}
 	}
-
-	@Override
-	protected void onPause() {
-		releaseCameraAndPreview();
-		super.onPause();
-	}
-
-	@Override
-	protected void onDestroy() {
-	    // TODO Auto-generated method stub
-	    super.onDestroy();
-	    releaseCameraAndPreview();
-	}
-
+	
+ 
 }
- */
+
