9ccab82fa3cc8a970e2fb7cc628e0e4848ca2be9
syhw <gabriel.synnaeve@gmail.com>
2013-05-12 09:12:54 +0200

    photos working with photo intent, problem was cyanogenmod

--- src/com/example/restophoto/FirstScreen.java
+++ src/com/example/restophoto/FirstScreen.java
@@ -25,7 +25,7 @@ import android.widget.Toast;
 public class FirstScreen extends Activity {
 	public static String DEBUG_TAG;
 	
-	/*
+	
 	private static final int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 100;
 	private Uri fileUri;
 
@@ -37,31 +37,35 @@ public class FirstScreen extends Activity {
 		// create Intent to take a picture and return control to the calling application
 		Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
 
-		fileUri = getOutputPhotoFileUri(); // create a file to save the image
-		intent.putExtra(MediaStore.EXTRA_OUTPUT, fileUri); // set the image file name
+		//fileUri = getOutputPhotoFileUri(); // create a file to save the image TODO does not work with cyanogen mod
+		//intent.putExtra(MediaStore.EXTRA_OUTPUT, fileUri); // set the image file name TODO does not work with cyanogenmod
+		intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(new File(
+				Environment.getExternalStoragePublicDirectory(
+				Environment.DIRECTORY_PICTURES), "photoresto.jpg")));
 		Toast.makeText(this, "Saving pics to:\n" +
-                 fileUri, Toast.LENGTH_LONG).show();
+                 "/sdcard/photoresto.jpg", //fileUri, 
+                 Toast.LENGTH_LONG).show();
 		// start the image capture Intent
 		startActivityForResult(intent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
 	}
 
-	private static Uri getOutputPhotoFileUri(){
+	private static Uri getOutputPhotoFileUri(){ // TODO unused with cyanogenmod
 		return Uri.fromFile(getOutputPhotoFile());
 	}
 
-	private static File getOutputPhotoFile(){
+	private static File getOutputPhotoFile(){ // TODO unused with cyanogenmod
 		// To be safe, you should check that the SDCard is mounted
 		// using Environment.getExternalStorageState() before doing this.
 
 		File mediaStorageDir = new File(Environment.getExternalStoragePublicDirectory(
-				Environment.DIRECTORY_PICTURES), "");//"MyCameraApp");
+				Environment.DIRECTORY_PICTURES), "photoresto");
 		// This location works best if you want the created images to be shared
 		// between applications and persist after your app has been uninstalled.
 
 		// Create the storage directory if it does not exist
 		if (! mediaStorageDir.exists()){
 			if (! mediaStorageDir.mkdirs()){
-				Log.d("MyCameraApp", "failed to create directory");
+				Log.d("photoresto", "failed to create directory");
 				return null;
 			}
 		}
@@ -81,6 +85,12 @@ public class FirstScreen extends Activity {
 	            // Image captured and saved to fileUri specified in the Intent
 	            Toast.makeText(this, "Image saved to:\n" +
 	                     data.getData(), Toast.LENGTH_LONG).show();
+	            // TODO now there is the image in Environment.getExternalStoragePublicDirectory(
+				// Environment.DIRECTORY_PICTURES) + "photoresto.jpg"
+	            // we can use location and photo (OCR + CV) to make API calls to various services 
+	            // http://stackoverflow.com/questions/3505930/make-an-http-request-with-android
+	            // and aggregate their results on the results.xml view/activity
+	            // /TODO
 	        } else if (resultCode == RESULT_CANCELED) {
 	            // User cancelled the image capture
 	        	Toast.makeText(this, "You cancelled the capture.", Toast.LENGTH_LONG).show();
@@ -90,227 +100,6 @@ public class FirstScreen extends Activity {
 	        }
 	    }
 	}
-	*/
-	
-	
-	private SurfaceView preview=null;
-	private SurfaceHolder previewHolder=null;
-	private Camera camera=null;
-	private boolean inPreview=false;
-	private boolean cameraConfigured=false; 
-	 
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		requestWindowFeature(Window.FEATURE_NO_TITLE);       
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_take_pic);
-
-		preview=(SurfaceView)findViewById(R.id.cameraPreview);
-		previewHolder=preview.getHolder();
-		previewHolder.addCallback(surfaceCallback);
-		previewHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
-	}
-
-	@Override
-	public void onResume() {
-		super.onResume();
-
-		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.GINGERBREAD) {
-//			Camera.CameraInfo info=new Camera.CameraInfo();
-//
-//      for (int i=0; i < Camera.getNumberOfCameras(); i++) {
-//        Camera.getCameraInfo(i, info);
-//
-//        if (info.facing == Camera.CameraInfo.CAMERA_FACING_FRONT) {
-//          camera=Camera.open(i);
-//        }
-//      }
-			camera=Camera.open();
-		}
-
-		if (camera == null) {
-			camera=Camera.open();
-		}
-
-		startPreview();
-	}
-
-	@Override
-	public void onPause() {
-		if (inPreview) {
-			camera.stopPreview();
-		}
-
-		camera.release();
-		camera=null;
-		inPreview=false;
-
-		super.onPause();
-	}
-
-	//@Override
-	//public boolean onCreateOptionsMenu(Menu menu) {
-	//  new MenuInflater(this).inflate(R.menu.options, menu);
-
-	//   return(super.onCreateOptionsMenu(menu));
-	//}
-
-	//@Override
-	//public boolean onOptionsItemSelected(MenuItem item) {
-	//	if (item.getItemId() == R.id.camera) {
-	//		if (inPreview) {
-	//			camera.takePicture(null, null, photoCallback);
-	//			inPreview=false;
-	//		}
-	//	}
-	//	return(super.onOptionsItemSelected(item));
-	//}
-
-	private Camera.Size getBestPreviewSize(int width, int height,
-			Camera.Parameters parameters) {
-		Camera.Size result=null;
-
-		for (Camera.Size size : parameters.getSupportedPreviewSizes()) {
-			if (size.width <= width && size.height <= height) {
-				if (result == null) {
-					result=size;
-				}
-				else {
-					int resultArea=result.width * result.height;
-					int newArea=size.width * size.height;
-
-					if (newArea > resultArea) {
-						result=size;
-					}
-				}
-			}
-		}
-
-		return(result);
-	}
-
-	private Camera.Size getSmallestPictureSize(Camera.Parameters parameters) {
-		Camera.Size result=null;
-
-		for (Camera.Size size : parameters.getSupportedPictureSizes()) {
-			if (result == null) {
-				result=size;
-			}
-			else {
-				int resultArea=result.width * result.height;
-				int newArea=size.width * size.height;
-
-				if (newArea < resultArea) {
-					result=size;
-				}
-			}
-		}
-
-		return(result);
-	}
-
-	private void initPreview(int width, int height) {
-		if (camera != null && previewHolder.getSurface() != null) {
-			try {
-				camera.setPreviewDisplay(previewHolder);
-			}
-			catch (Throwable t) {
-				Log.e("PreviewDemo-surfaceCallback",
-						"Exception in setPreviewDisplay()", t);
-				Toast.makeText(FirstScreen.this, t.getMessage(),
-						Toast.LENGTH_LONG).show();
-			}
-
-			if (!cameraConfigured) {
-				Camera.Parameters parameters=camera.getParameters();
-				Camera.Size size=getBestPreviewSize(width, height, parameters);
-				Camera.Size pictureSize=getSmallestPictureSize(parameters);
-
-				if (size != null && pictureSize != null) {
-					parameters.setPreviewSize(size.width, size.height);
-					parameters.setPictureSize(pictureSize.width,
-							pictureSize.height);
-					parameters.setPictureFormat(ImageFormat.JPEG);
-					camera.setParameters(parameters);
-					cameraConfigured=true;
-				}
-			}
-		}
-	}
-
-	private void startPreview() {
-		if (cameraConfigured && camera != null) {
-			camera.startPreview();
-			inPreview=true;
-		}
-	}
-
-	SurfaceHolder.Callback surfaceCallback=new SurfaceHolder.Callback() {
-		public void surfaceCreated(SurfaceHolder holder) {
-			// no-op -- wait until surfaceChanged()
-		}
-
-		public void surfaceChanged(SurfaceHolder holder, int format,
-				int width, int height) {
-			initPreview(width, height);
-			startPreview();
-		}
-
-		public void surfaceDestroyed(SurfaceHolder holder) {
-			// no-op
-		}
-	};
-
-	Camera.PictureCallback photoCallback=new Camera.PictureCallback() {
-		public void onPictureTaken(byte[] data, Camera camera) {
-			new SavePhotoTask().execute(data);
-			//Toast.makeText(FirstScreen.this, "Saved photo in:\n" +
-			//		Environment.getExternalStoragePublicDirectory(
-			//				Environment.DIRECTORY_PICTURES).getName(), 
-			//				Toast.LENGTH_LONG).show();
-			Log.d("saved a photo in dir", Environment.getExternalStoragePublicDirectory(
-							Environment.DIRECTORY_PICTURES).getName());
-			camera.startPreview();
-			inPreview=true;
-		}
-	};
-
-	class SavePhotoTask extends AsyncTask<byte[], String, String> {
-		@Override
-		protected String doInBackground(byte[]... jpeg) {
-			File photo=
-					new File(Environment.getExternalStoragePublicDirectory(
-							Environment.DIRECTORY_PICTURES),
-							//Environment.getExternalStorageDirectory(),
-							"photoresto.jpg");
-
-			if (photo.exists()) {
-			//	Toast.makeText(FirstScreen.this, "Deleted photo:\n" +
-			//			photo.getAbsolutePath(), Toast.LENGTH_LONG).show();
-				Log.d("deleted a photo", photo.getAbsolutePath());
-				photo.delete();
-			}
-
-			try {
-				FileOutputStream fos=new FileOutputStream(photo.getPath());
-
-				fos.write(jpeg[0]);
-				fos.close();
-			}
-			catch (java.io.IOException e) {
-				Log.e("PictureDemo", "Exception in photoCallback", e);
-			}
-
-			return(null);
-		}
-	}
-
-	public void onClick(View view) {
-		//if (inPreview) {
-			camera.takePicture(null, null, photoCallback);
-				//new PhotoHandler(getApplicationContext()));
-		//}
-	}
- 
+ 	
 }
 
